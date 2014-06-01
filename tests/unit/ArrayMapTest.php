972755210c8a9c12b782a0b79b2756f224c42f82
Petr Grishin <petr.grishin@grishini.ru>
2014-06-01 11:25:51 +0400

    Implementing unit test

---
+++ tests/unit/ArrayMapTest.php
@@ -0,0 +1,46 @@
+<?php
+use \PetrGrishin\ArrayMap\ArrayMap;
+
+/**
+ * @author Petr Grishin <petr.grishin@grishini.ru>
+ */
+
+class ArrayMapTest extends PHPUnit_Framework_TestCase {
+
+    public function testCreateInstance() {
+        $instance = new ArrayMap();
+        $this->assertInstanceOf(ArrayMap::className(), $instance);
+    }
+
+    public function testSimpleMapping() {
+        $original = array(1, 2, 3);
+        $instance = ArrayMap::create($original);
+        $instance->map(function ($value) {
+            return $value * 2;
+        });
+        $this->assertEquals(array(2, 4, 6), $instance->getArray());
+    }
+
+    public function testKeyMapping() {
+        $original = array(1 => 1, 2 => 2, 3 => 3);
+        $instance = ArrayMap::create($original);
+        $instance->map(function ($value, $key) {
+            return array(($key - 1) => $value * 2);
+        });
+        $this->assertEquals(array(0 => 2, 1 => 4, 2 => 6), $instance->getArray());
+    }
+
+    public function testMergeWith() {
+        $original = array(1, 2, 3);
+        $instance = ArrayMap::create($original);
+        $instance->mergeWith(array(4, 5, 6), false);
+        $this->assertEquals(array(1, 2, 3, 4, 5, 6), $instance->getArray());
+    }
+
+    public function testRecursiveMergeWith() {
+        $original = array('a' => array(1), 'b', 'c');
+        $instance = ArrayMap::create($original);
+        $instance->mergeWith(array('a' => array(2), 'd', 'e'));
+        $this->assertEquals(array('a' => array(1, 2), 'b', 'c', 'd', 'e'), $instance->getArray());
+    }
+}
